9115cd0ff0e9a6f86f0128663e22b4b394da26cf
Nish Joseph <nish.joseph@leapaust.com.au>
2020-07-12 21:48:15 +1000

    Add license info and description.

--- BackupResults/script.py
+++ BackupResults/script.py
@@ -1,3 +1,29 @@
+"""
+main.py
+
+Copyright 2020 LEAP Australia Pty Ltd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+Author:
+    Khesh Selvaganapathi
+    Nish Joseph
+
+Discription:
+    ACT backup ANSYS Mechanical results with options to append timestamps
+    design point number and copy of input file.
+"""
+
 import os
 import shutil
 import datetime
